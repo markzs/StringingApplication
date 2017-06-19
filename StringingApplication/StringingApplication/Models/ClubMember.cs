f52b6d444952fff1996680656e6a996ba63014be
Mark Sansait <marks@ocisi.com>
2017-06-19 09:53:08 -0500

    Added entityframework connection

---
+++ StringingApplication/StringingApplication/Models/ClubMember.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StringingApplication.Models
+{
+    public class ClubMember
+    {
+        public int LastName { get; set; }
+        public int FirstName { get; set; }
+    }
+}
